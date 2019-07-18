8a8bd4f4b79a20dab769e9f37fb26b724f37c3d1
Yoyo Lee <lee.s.yoyo0409@gmail.com>
2019-07-18 17:07:38 +0800

    尷尬...這個才是對的...

--- 1a2b.py
+++ 1a2b.py
@@ -1,39 +1,39 @@
 import random
-ans=[]
-while len(ans)!=4:
-    x=random.randint(1,9)
+ans = []
+while len(ans) != 4:
+    x = random.randint(1, 9)
     if int(x) not in ans:
         ans.append(x)
 print(ans)
 
-a=0
-b=0
-while a<4:
-    gus=input("incert:")
-    alist=[int(x) for x in gus]
-    a=0
-    b=0
+a = 0
+b = 0
+while a < 4:
+    gus = input("incert:")
+    alist = [int(x) for x in gus]
+    a = 0
+    b = 0
     if alist[0] in ans:
-        if w1 == alist[0]:
-            a+=1
+        if ans[0] == alist[0]:
+            a += 1
         else:
-            b+=1
+            b += 1
     if alist[1] in ans:
-        if w2 == alist[1]:
-            a+=1
+        if ans[1] == alist[1]:
+            a += 1
         else:
-          b+=1    
+            b += 1
     if alist[2] in ans:
-        if w3 == alist[2]:
-            a+=1
+        if ans[2] == alist[2]:
+            a += 1
         else:
-            b+=1
+            b += 1
     if alist[3] in ans:
-        if w4 == alist[3]:
-            a+=1
+        if ans[3] == alist[3]:
+            a += 1
         else:
-            b+=1
-    print(a,"A",b,"B")
-    
-    
+            b += 1
+    print(a, "A", b, "B")
+
+
 print("4A0B")
