84e369e020078f1ba63c07b6b180a5556f0c3324
Yoyo Lee <lee.s.yoyo0409@gmail.com>
2019-07-18 20:56:10 +0800

    用一次function就能回傳a,b值 n/還有把incert改insert

--- 1a2b.py
+++ 1a2b.py
@@ -5,29 +5,21 @@ while len(ans) != 4:
     if int(x) not in ans:
         ans.append(x)
 print(ans)
-p=0
-def yee(y):
-    if alist[y] in ans:
-        if ans[y] == alist[y]:
-            return 1
-        else:
-            return 2
-alot=0
-blot=0
-while alot < 4:
+alot = []
+blot = []
+
+def gusmod(*onetofour):
+    for gusnm in onetofour:
+      if guslist[gusnm] in ans:
+            if ans[gusnm] == guslist[gusnm]:
+                alot.append(gusnm)
+            else:
+                blot.append(gusnm)
+while len(alot) < 4:
     gus = input("insert:")
-    alist = [int(x) for x in gus]
-    alot = 0
-    blot = 0
-    y = 0
-    while y<4:
-        if yee(y) == 1:
-            alot+=1
-        elif yee(y) == 2:
-            blot+=1     
-        else:
-            p=1
-        y+=1
-    print(alot, "A", blot, "B")
-
-
+    guslist = [int(x) for x in gus]
+    print(guslist)
+    alot = []
+    blot = []
+    gusmod(0,1,2,3)
+    print(len(alot), "A", len(blot), "B")
---
+++ p.py
@@ -0,0 +1,4 @@
+def gusmod(*onetofour):
+    for gusnm in onetofour:
+        print(gusnm)
+gusmod(1,2,3,4)
