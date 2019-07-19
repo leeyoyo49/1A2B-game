f16cd3d2b7a48baa2571cd5f47fc459bdbd749d7
Yoyo Lee <lee.s.yoyo0409@gmail.com>
2019-07-19 21:22:56 +0800

    ans的部分用List的shuffle替代random亂數

--- 1a2b.py
+++ 1a2b.py
@@ -1,11 +1,11 @@
 import random
-ans = []
-while len(ans) != 4:
-    x = random.randint(0, 9)
-    if int(x) not in ans:
-        ans.append(x)
+ans = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+random.shuffle(ans)
+del ans[4:10]
 print(ans)
 count_of_a = count_of_b = 0
+
+
 def compare(ans, guslist):
     count_of_a = count_of_b = 0
     for y in range(4):
@@ -14,9 +14,11 @@ def compare(ans, guslist):
                 count_of_a += 1
             else:
                 count_of_b += 1
-    return count_of_a,count_of_b
+    return count_of_a, count_of_b
+
+
 while count_of_a < 4:
     gus = input("insert:")
     guslist = [int(x) for x in gus]
-    count_of_a,count_of_b = compare(ans, guslist)
+    count_of_a, count_of_b = compare(ans, guslist)
     print(count_of_a, "A", count_of_b, "B")
