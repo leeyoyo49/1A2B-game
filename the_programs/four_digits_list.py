a7337abc461c5b5c00d06b9b0f8629d1c9bc8cfa
Yoyo Lee <lee.s.yoyo0409@gmail.com>
2019-08-06 20:28:12 +0800

    change the whole game into a class

--- the_programs/four_digits_list.py
+++ the_programs/four_digits_list.py
@@ -1,104 +1,91 @@
 import random
-count_of_a = count_of_b = 0
+import itertools
+a = 0
 
 
-def compare(ans, guslist):
-    count_of_a = count_of_b = 0
-    for y in range(4):
-        if guslist[y] in ans:
-            if guslist[y] == ans[y]:
-                count_of_a += 1
-            else:
-                count_of_b += 1
-    return count_of_a, count_of_b
-
-
-print('===================================')
-
-
-class Number:
+class Game:
     def __init__(self):
-        pass
-    def random(self):
-        ans = random.choice(allthenumlist)
-        return ans
+        print('''===================================
+type "quit" to leave the game''')
+        self.p_ct_a = self.computer_a = 0
+        self.all_the_maybe_ans = (list(itertools.permutations(range(10), 4)))
+        self.the_rest_of_maybe_ans = self.all_the_maybe_ans
+        self.player_ans = random.choice(self.all_the_maybe_ans)
+        self.the_history_box = {}
 
-guslist = allthenumlist = []
-player_give_ab = ""
-# 資料庫(一行就能解決的事我寫了10行)
+    def compare(self, ans, the_gus):
+        guslist = [int(x) for x in the_gus]
+        count_of_a = count_of_b = 0
+        for run_four_times in range(4):
+            if guslist[run_four_times] in ans:
+                if guslist[run_four_times] == ans[run_four_times]:
+                    count_of_a += 1
+                else:
+                    count_of_b += 1
+        return count_of_a, count_of_b
 
-for y in range(123, 9877):
-    thenumbers = [int(n) for n in str(y)]
-    if len(thenumbers) < 4:
-        thenumbers.insert(0, 0)
-    if len(thenumbers) != len(set(thenumbers)):
-        continue
-    allthenumlist.append(thenumbers)
-player_give_a = player_give_b = 0
-the_history_box = {}
-
-ans = Number()
-ans = ans.random()
-
-# 遊戲開始
-while count_of_a < 4 and player_give_a < 4:
-    gus = input("insert your guess(four numbers):")
-    # 處理輸入錯誤
-    while True:
-        try:
-            guslist = [int(x) for x in gus]
-            if len(guslist) > 4:
-                print('you can only insert four numbers')
-                gus = input("insert your guess:")
-                continue
-            else:
+    def player_guess(self):
+        while True:
+            player_give_guess = input("insert your guess(four numbers):")
+            try:
+                if player_give_guess == 'quit':
+                    return 5
+                self.p_ct_a, p_ct_b = self.compare(
+                    self.player_ans, player_give_guess)
+                print("the result is ", self.p_ct_a, "A", p_ct_b, "B")
                 break
-        except:
-            print('you insert the wrong thing,try again')
-            gus = input("insert your guess:")
-            continue
-    count_of_a, count_of_b = compare(ans, guslist)
-    print("the result is:", count_of_a, "A", count_of_b, "B")
-    if count_of_a == 4:
-        break
-    player_give_a = player_give_b = 0
-    notallthenumlist = []
+            except:
+                print('you insert the wrong thing ,insert it again')
+
+    def com_give_gus(self):
+        self.comgus = random.choice(self.the_rest_of_maybe_ans)
+        print("I guess your number is ", self.comgus)
+
+    def com_solve_player_ans(self):
+        only_for_append_list = []
+        while True:
+            self.player_give_ans = input("show me the result: ")
+            try:
+                if self.player_give_ans == 'quit':
+                    return 5
+                self.computer_a = int(self.player_give_ans[0])
+                for x in self.the_rest_of_maybe_ans:
+                    com_count_of_a, com_count_of_b = self.compare(
+                        self.comgus, x)
+                    if com_count_of_a == int(self.player_give_ans[0]) and com_count_of_b == int(self.player_give_ans[2]):
+                        only_for_append_list.append(x)
+                        self.the_rest_of_maybe_ans = only_for_append_list
+                print("there are", len(only_for_append_list), "possiblilties")
+                break
+            except:
+                print('you insert the wrong thing ,insert it again')
+
+    def the_whole_history_box(self):
+        com_gus_str = map(str, self.comgus)
+        self.the_history_box.update(
+            {''.join(com_gus_str): self.player_give_ans})
+        print(self.the_history_box)
+
+    def repeat(self):
+        if self.p_ct_a == 4:
+            print("you won the game")
+            return 4
+        elif self.computer_a == 4:
+            print("I won the game haha")
+            return 4
+        else:
+            return 0
+
+
+while a < 5:
     a = 0
-# 電腦產亂數給玩家猜，順便紀錄
-    com_gus = random.choice(allthenumlist)
-    com_gus_str = map(str, com_gus)
-    print("I guess your number is ", com_gus)
-    while True:
-        try:
-            player_give_ab = input("show me the result(?a?b):")
-            player_give_a = int(player_give_ab[0])
-            player_give_b = int(player_give_ab[2])
-            if len(player_give_ab) < 4:
-                print("show me the right answer!!")
-                continue
-            else:
-                break
-        except:
-            print('you insert the wrong thing ,insert it again')
-            player_give_ab = input("show me the result(?a?b):")
-            a = 1
-    the_history_box.update({''.join(com_gus_str): player_give_ab})
-# 電腦把不可能的值刪除
-    for y in allthenumlist:
-        com_count_of_a, com_count_of_b = compare(com_gus, y)
-        if com_count_of_a == player_give_a and com_count_of_b == player_give_b:
-            notallthenumlist.append(y)
-            allthenumlist = notallthenumlist
-    # 玩家作弊
-    if len(notallthenumlist) == 0:
-        print("you cheated!!")
-        break
-    # 顯示出有神馬可能性
-    print(the_history_box, "there are", len(notallthenumlist),
-          "possibilities"+'\n'+"==================================================")
-
-# 獲勝條件
-if player_give_a == 4:
-    print("haha I win loser")
-if count_of_a == 4:
-    print("it must be a mistake that i lost")
+    game = Game()
+    while a < 4:
+        a = game.player_guess()
+        if a == 5:
+            break
+        game.com_give_gus()
+        a = game.com_solve_player_ans()
+        game.the_whole_history_box()
+        a = game.repeat()
+        print('===================================')
